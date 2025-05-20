45eed7e767f48a7839fa5a5c74a0fc5725a7a89f
darellanodev <darellano.aliaga@gmail.com>
2025-05-20 20:58:15 +0200

    refactor: Replace card implementation with a web component (#3)

---
+++ js/blog-card.js
@@ -0,0 +1,32 @@
+class BlogCard extends HTMLElement {
+  constructor() {
+    super();
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = `
+      <link rel="stylesheet" href="./css/style.css">
+      <article class="blog-card">
+        <div class="blog-card__content">
+          <img class="blog-card__image" src="${
+            this.getAttribute("image") || ""
+          }" alt="Article illustration">
+          <p class="blog-card__challenge">
+            ${this.getAttribute("challenge") || ""}
+          </p>
+          <a class="blog-card__title">${this.getAttribute("title") || ""}</a>
+          <div class="blog-card__description">
+            ${this.getAttribute("description") || ""}
+          </div>
+          <div class="blog-card__author">
+            <img src="${this.getAttribute("author-image") || ""}" alt="${
+      this.getAttribute("author") || ""
+    }" class="blog-card__author-image">
+            <div class="blog-card__author-name">${
+              this.getAttribute("author") || ""
+            }</div>
+          </div>
+        </div>
+      </article>
+    `;
+  }
+}
+customElements.define("blog-card", BlogCard);
