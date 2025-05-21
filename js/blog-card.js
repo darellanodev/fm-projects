d2a8b48df2b65c2c809bd37e235aa48de527972f
darellanodev <darellano.aliaga@gmail.com>
2025-05-21 20:51:25 +0200

    feat: Add a link to the author's name and format blog-card.js with prettier (closes #9)

--- js/blog-card.js
+++ js/blog-card.js
@@ -1,34 +1,30 @@
 class BlogCard extends HTMLElement {
   constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
+    super()
+    const shadow = this.attachShadow({ mode: 'open' })
     shadow.innerHTML = `
       <link rel="stylesheet" href="./css/style.css">
       <article class="blog-card">
         <div class="blog-card__content">
-          <img class="blog-card__image" src="${
-            this.getAttribute("image") || ""
-          }" alt="Article illustration">
+          <img class="blog-card__image" src="${this.getAttribute('image') || ''}" alt="Article illustration" title="${this.getAttribute('title') || ''}">
           <p class="blog-card__challenge">
-            ${this.getAttribute("challenge") || ""}
+            ${this.getAttribute('challenge') || ''}
           </p>
-          <a class="blog-card__title" href="${
-            this.getAttribute("href") || "#"
-          }">${this.getAttribute("title") || ""}</a>
+          <a class="blog-card__title" href="${this.getAttribute('href') || '#'}">${this.getAttribute('title') || ''}</a>
           <div class="blog-card__description">
-            ${this.getAttribute("description") || ""}
+            ${this.getAttribute('description') || ''}
           </div>
           <div class="blog-card__author">
-            <img src="${this.getAttribute("author-image") || ""}" alt="${
-      this.getAttribute("author") || ""
-    }" class="blog-card__author-image">
-            <div class="blog-card__author-name">${
-              this.getAttribute("author") || ""
-            }</div>
+            <img src="${this.getAttribute('author-image') || ''}" alt="${this.getAttribute('author') || ''}" class="blog-card__author-image" title="${
+      this.getAttribute('author') || ''
+    }">
+            <a class="blog-card__author-name" href="${this.getAttribute('author-href') || '#'}" title="${this.getAttribute('author') || ''}">${
+      this.getAttribute('author') || ''
+    }</a>
           </div>
         </div>
       </article>
-    `;
+    `
   }
 }
-customElements.define("blog-card", BlogCard);
+customElements.define('blog-card', BlogCard)
