36f3202e7f07d24be3acebdde6e0f67231c93ddd
darellanodev <darellano.aliaga@gmail.com>
2025-06-29 13:23:33 +0200

    feat(project4): Use clamp for h1 title and move footer styles to App.vue

--- 04-recipe-page-main/src/App.vue
+++ 04-recipe-page-main/src/App.vue
@@ -2,7 +2,7 @@
   <main>
     <RecipeCard />
   </main>
-  <footer class="footer">
+  <footer>
     Challenge by <a href="#">Frontend Mentor</a>. Coded by <a href="#">Your Name Here</a>.
   </footer>
 </template>
@@ -12,9 +12,14 @@ import RecipeCard from './components/RecipeCard.vue'
 </script>
 
 <style scoped>
-.footer {
+footer {
   text-align: center;
   margin-top: var(--spacing-xl);
+  margin-bottom: var(--spacing-xl);
   font-size: var(--font-size-sm);
+}
+
+footer > a {
+  color: var(--color-rose-800);
 }
 </style>
